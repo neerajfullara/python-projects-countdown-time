d95683bf7657cc842a2b430bd2c0cdd0ca58728b
Neeraj Fullara <75372853+neerajfullara@users.noreply.github.com>
2022-11-16 22:03:26 +0530

    Create timer.py
    Added some base code.

---
+++ countdown-timer/timer.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+root = Tk()
+root.title('CountDown Timer')
+root.iconbitmap('C:/Users/neera/Desktop/python-projects/resources/logo.ico')
+root.geometry("500x300")
+
+root.mainloop()
