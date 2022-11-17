a14b734745863298a85b612a9a1a057e92c18f6a
Neeraj Fullara <75372853+neerajfullara@users.noreply.github.com>
2022-11-17 21:57:39 +0530

    Update timer.py
    imported time.
    added clock function.
    added update function.
    styled text for clock.

--- countdown-timer/timer.py
+++ countdown-timer/timer.py
@@ -1,8 +1,27 @@
 from tkinter import *
+import time # For clock
 
 root = Tk()
 root.title('CountDown Timer')
-root.iconbitmap('C:/Users/neera/Desktop/python-projects/resources/logo.ico')
+root.iconbitmap('C:/Users/neera/Desktop/python-projects/countdown-timer/resources/logo.ico')
 root.geometry("500x300")
+
+# Added the clock Functionality
+def clock():
+    hour = time.strftime("%H")
+    minute = time.strftime("%M")
+    second = time.strftime("%S")
+
+    C_label.config(text=hour +":" + minute + ":" + second)
+    C_label.after(1000,clock)
+
+def  update():
+    C_label.config(text="")
+
+# Styling of Clock
+C_label = Label(root, text="", font=("Georgia", 40),fg="black")
+C_label.pack(pady=30)
+
+clock()
 
 root.mainloop()
